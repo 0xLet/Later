0d8697de9c8a047c5e3ee704cd356282368b2ae2
zmeriksen <zmeriksen@icloud.com>
2020-10-02 17:03:52 -0500

    Added static

--- Sources/Later/Later+EventLoopFuture.swift
+++ Sources/Later/Later+EventLoopFuture.swift
@@ -8,19 +8,19 @@
 import NIO
 
 public extension Later {
-    func andAllComplete<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<Void> {
+    static func andAllComplete<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<Void> {
         EventLoopFuture.andAllComplete(futures, on: Later.default.ev)
     }
     
-    func andAllSucceed<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<Void> {
+    static func andAllSucceed<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<Void> {
         EventLoopFuture.andAllSucceed(futures, on: Later.default.ev)
     }
     
-    func whenAllComplete<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<[Result<Value, Error>]> {
+    static func whenAllComplete<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<[Result<Value, Error>]> {
         EventLoopFuture.whenAllComplete(futures, on: Later.default.ev)
     }
     
-    func whenAllSucceed<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<[Value]> {
+    static func whenAllSucceed<Value>(_ futures: [EventLoopFuture<Value>]) -> EventLoopFuture<[Value]> {
         EventLoopFuture.whenAllSucceed(futures, on: Later.default.ev)
     }
 }
